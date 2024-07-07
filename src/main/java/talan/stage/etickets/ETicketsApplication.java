cdf620f0f4a2e57d32247e6486c8163f17c3d5e2
Hadil Hammami <hadilhammami670@gmail.com>
2024-07-07 14:35:32 +0200

    initialize project

---
+++ src/main/java/talan/stage/etickets/ETicketsApplication.java
@@ -0,0 +1,13 @@
+package talan.stage.etickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ETicketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/talan/stage/etickets/ETicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package talan.stage.etickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
